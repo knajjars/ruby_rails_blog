28c2aa0b6b35c1d5be6edbcb0ff77cee58696797
Khalil Najjar <knajjars@gmail.com>
2020-11-22 11:05:19 +0100

    blogs controlller changes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -62,13 +62,14 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
