8211513a3c26b6407d7c613d14260e7f8ac0dfef
Khalil Najjar <knajjars@gmail.com>
2020-11-23 11:19:11 +0100

    added status to blogs and toggle functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :portfolios
-  resources :blogs
+  resources :blogs do
+    member do
+      post :toggle_status
+    end
+  end
 end
